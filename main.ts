d9329078d1c889866f1f5a50f87d825f17476ca8
Denisoed <denisoed@gmail.com>
2021-01-16 00:20:26 +0600

    init project

---
+++ main.ts
@@ -0,0 +1,167 @@
+import { strict } from 'assert';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		console.log('loading plugin');
+
+		await this.loadSettings();
+
+		this.addRibbonIcon('dice', 'Sample Plugin', () => {
+			new Notice('This is a notice!');
+		});
+
+    this.addStatusBarItem().setText('Status Bar Text');
+    
+    this.addCommand({
+      id: 'better-toggle-todo',
+      name: 'Toggle to-do lists',
+      callback: () => this.checkTexts(),
+      hotkeys: [{
+          modifiers: ['Mod', 'Shift'],
+          key: 'l'
+        }
+      ]
+    });
+
+		this.addCommand({
+			id: 'open-sample-modal',
+			name: 'Open Sample Modal',
+			checkCallback: (checking: boolean) => {
+				let leaf = this.app.workspace.activeLeaf;
+				if (leaf) {
+					if (!checking) {
+						new SampleModal(this.app).open();
+					}
+					return true;
+				}
+				return false;
+      }
+		});
+
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		this.registerCodeMirror((cm: CodeMirror.Editor) => {
+			console.log('codemirror', cm);
+		});
+
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+		console.log('unloading plugin');
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+  }
+  
+  async checkTexts() {
+    const activeLeaf: any = this.app.workspace.activeLeaf;
+    const editor = activeLeaf.view.sourceMode.cmEditor;
+    const text = editor.getValue();
+    const url = 'https://speller.yandex.net/services/spellservice.json/checkTexts';
+    const formData = new FormData();
+    formData.append('text', text);
+    formData.append('lang', 'en');
+    postData(url, formData)
+      .then((data) => {
+        const res = data[0].map(function(item: any) {
+          return item['word'];
+        });
+        var regexFromMyArray = new RegExp(res.join("|"), 'gi');
+        console.log(regexFromMyArray, '!!!!');
+        
+        this.search(regexFromMyArray);
+      });
+  }
+
+  search(val: any) {
+    const activeLeaf: any = this.app.workspace.activeLeaf;
+    const editor = activeLeaf.view.sourceMode.cmEditor;
+    var searchQuery = new RegExp(val)
+    var cursor = editor.getSearchCursor(searchQuery);
+    console.log(cursor);
+    while (cursor.findNext()) {
+      editor.markText(
+        cursor.from(),
+        cursor.to(),
+        { className: 'highlight' }
+      );
+    }
+  }
+}
+
+
+// Пример отправки POST запроса:
+async function postData(url = '', data: any) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    body: data // body data type must match "Content-Type" header
+  });
+  return await response.json(); // parses JSON response into native JavaScript objects
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		let {contentEl} = this;
+		contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		let {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue('')
+				.onChange(async (value) => {
+					console.log('Secret: ' + value);
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
