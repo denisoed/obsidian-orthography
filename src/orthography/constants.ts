4b833e685d2bddfb6047cae7fc303773c71919de
Denisoed <denisoed@gmail.com>
2021-01-17 01:08:33 +0600

    fixed logic for tooltip | added animation

--- src/orthography/constants.ts
+++ src/orthography/constants.ts
@@ -1,5 +1,6 @@
 export const TOOLTIP_CSS_CLASS = 'obsidian-orthography-tooltip';
 export const TOOLTIP_VISIBLE_CSS_CLASS =
   'obsidian-orthography-tooltip--visible';
+export const TOOLTIP_HINT_CSS_CLASS = 'obsidian-orthography-tooltip-hint';
 export const HIGHLIGHT_CSS_CLASS = 'obsidian-orthography-highlight';
 export const RUNNER_CSS_CLASS = 'obsidian-orthography-runner';
--- src/orthography/orthographyTooltip.ts
+++ src/orthography/orthographyTooltip.ts
@@ -1,6 +1,7 @@
 import {
   TOOLTIP_CSS_CLASS,
   TOOLTIP_VISIBLE_CSS_CLASS,
+  TOOLTIP_HINT_CSS_CLASS,
   HIGHLIGHT_CSS_CLASS
 } from './constants';
 
@@ -8,8 +9,7 @@ interface IOrthographyTooltip {
   init(): void;
 }
 
-export class OrthographyTooltip
-  implements IOrthographyTooltip {
+export class OrthographyTooltip implements IOrthographyTooltip {
   private tooltip: any;
 
   public init(): void {
@@ -26,50 +26,54 @@ export class OrthographyTooltip
 
   private setDataToTooltip(word: string): void {
     const data = JSON.parse(localStorage.getItem('obsidian-orthography'));
-    const hints = data.find((d: any) => word === d.word ? d : null);
+    const hints = data.find((d: any) => (word === d.word ? d : null));
     if (hints.hasOwnProperty('s')) {
-      this.createHintButton(hints.s);
+      this.appendHintButton(hints.s);
     }
   }
 
   private toggleTooltip(event: any): void {
     if (event.type === 'mouseover') {
       if (event.target.className.trim() === HIGHLIGHT_CSS_CLASS) {
+        this.tooltip.innerHTML = '';
         this.setDataToTooltip(event.target.innerText);
         this.tooltip.classList.add(TOOLTIP_VISIBLE_CSS_CLASS);
         this.tooltip.style.left = this.getLeftPos(event);
         this.tooltip.style.top = this.getTopPos(event);
       }
     }
-    if (event.type === 'mouseout') {
+    if (
+      !event.target.className.trim().includes(TOOLTIP_CSS_CLASS) &&
+      event.target.className.trim() !== HIGHLIGHT_CSS_CLASS
+    ) {
       this.tooltip.classList.remove(TOOLTIP_VISIBLE_CSS_CLASS);
       this.tooltip.innerHTML = '';
     }
   }
 
   private getLeftPos(event: any) {
-    if (
-      event.pageX + this.tooltip.clientWidth + 10 <
-      document.body.clientWidth
-    ) {
-      return event.pageX + 10 + 'px';
+    const word = event.target.getBoundingClientRect();
+    if (word.x + this.tooltip.clientWidth + 10 < document.body.clientWidth) {
+      return word.x + 'px';
     }
     return document.body.clientWidth + 5 - this.tooltip.clientWidth + 'px';
   }
 
   private getTopPos(event: any) {
+    const word = event.target.getBoundingClientRect();
     if (
-      event.pageY + this.tooltip.clientHeight + 10 <
+      word.y + this.tooltip.clientHeight + word.height <
       document.body.clientHeight
     ) {
-      return event.pageY + 10 + 'px';
+      return word.y + word.height + 'px';
     }
-    return document.body.clientHeight + 5 - this.tooltip.clientHeight + 'px';
+    return document.body.clientHeight + 10 - this.tooltip.clientHeight + 'px';
   }
 
-  private createHintButton(hints: []) {
+  private appendHintButton(hints: []) {
     hints.forEach((h: string) => {
       const hint = document.createElement('button');
+      hint.classList.add(TOOLTIP_HINT_CSS_CLASS);
       hint.innerText = h;
       this.tooltip.appendChild(hint);
     });
