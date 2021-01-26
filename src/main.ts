84516725c3d9b7a5557524ce83e6d403675d7fd4
Denisoed <denisoed@gmail.com>
2021-01-26 18:55:48 +0600

    replaced unload to onunload

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ export default class OrthographyPlugin extends Plugin {
     });
   }
 
-  unload() {
+  onunload() {
     this.emitter.off('onUpdateSettings', this.onUpdateSettings.bind(this));
     this.runner.destroy();
     this.tooltip.destroy();
